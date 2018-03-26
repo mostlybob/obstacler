a3eb266bc34ff51a52e438a680d11b3e36817fb0
Bob Campbell <mostlybob@gmail.com>
2018-03-26 10:14:00 -0500

    refactor method for getting the distance

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -13,18 +13,8 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
- 
+  duration = checkPingDuration();
+
   // convert the time into a distance
   cm = (duration/2) / 29.1;
   inches = (duration/2) / 74; 
@@ -36,5 +26,19 @@ void loop() {
   Serial.println();
   
   delay(250);
+}
 
+long checkPingDuration(){
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+
+  return pulseIn(echoPin, HIGH);
 }
